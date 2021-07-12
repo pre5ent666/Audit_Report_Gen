1bc97df0c48e600f6c54ceb0d1abea11e9f3d6dd
pre5ent666 <75908533+pre5ent666@users.noreply.github.com>
2021-07-12 10:12:23 +0800

    Update Audit_Report_Gen.py

--- Audit_Report_Gen.py
+++ Audit_Report_Gen.py
@@ -3,18 +3,17 @@ from openpyxl import Workbook
 import re
 
 def matching(wb1, wb2, Week, tail, order1, order2, output_order):
-    # order1 = ['Main', 'Product', 'Product', 'Main']
-    # order2 = ['Main', 'Product', 'Point', 'STR']
-    # output_order = ['Main', 'Product', 'W25P_Point', 'W25M_STR']
     wb_out = Workbook()
     for j in range(len(order1)):
         sheet1 = wb1[order1[j]]
         sheet2 = wb2[order2[j]]
         ws = wb_out.create_sheet(output_order[j])
-        i = 1
+        i = 0
         for row1 in sheet1.iter_rows(max_col=tail, values_only=True):
             cell_output = cell_data(row1)
             match = False
+            if cell_output[0] == 'none':
+                break
             for row2 in sheet2.iter_rows(max_col=2, values_only=True):
                 cell_temp = cell_data(row2)
                 if str(cell_output[0]).lower() == str(cell_temp[0]).lower():
@@ -23,10 +22,10 @@ def matching(wb1, wb2, Week, tail, order1, order2, output_order):
                     ws.append(cell_output)
                     break
             if match == False:
-                print(cell_output)
+                cell_output.append('none')
                 ws.append(cell_output)
-            print(i)
             i = i + 1
+        print(output_order[j] + ': ' + str(i-1))
     wb_out.save(folder + 'Audit_Report_W' + str(Week) + '.xlsx')
 
 def cell_data(row):
@@ -38,15 +37,17 @@ def cell_data(row):
                 cells.append(cell)
         return cells
 
+#--------------------------------------------- Input Settings -------------------------------------------------
 tail = 2
-Week = 27
-order1 = ['Main', 'Product', 'STR']
-order2 = ['Main', 'Product', 'STR']
-output_order = ['Main', 'Product', 'STR']
-folder = 'C:\\Users\\Yvonne\\Documents\\Results\\'
-# folder = 'C:\\Users\\Yvonne\\Downloads\\'
+Week = 28
+order1 = ['Main', 'Product', 'STR', 'Product', 'Main']    # This week
+order2 = ['Main', 'Product', 'STR', 'Main', 'Product']    # Last week
+output_order = ['Main', 'Product', 'STR', 'W28P_W27M', 'W28M_W27P']
 
-wb1 = load_workbook(folder + '2021_W' + str(Week-1) + '.xlsx')
-wb2 = load_workbook(folder + '2021_W' +  str(Week)  + '.xlsx')
+folder = 'C:\\Users\\Yvonne\\Documents\\Results\\'
+#--------------------------------------------------------------------------------------------------------------
+
+wb1 = load_workbook(folder + '2021_W' +  str(Week)  + '.xlsx')
+wb2 = load_workbook(folder + '2021_W' + str(Week-1) + '.xlsx')
 
 matching(wb1, wb2, Week, tail, order1, order2, output_order)
