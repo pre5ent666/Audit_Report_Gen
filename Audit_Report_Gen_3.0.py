09a4aa4eb642ee86a6fdcf8113632c75100825e7
pre5ent666 <75908533+pre5ent666@users.noreply.github.com>
2021-12-29 02:14:48 +0800

    Update Audit_Report_Gen_3.0.py

--- Audit_Report_Gen_3.0.py
+++ Audit_Report_Gen_3.0.py
@@ -3,8 +3,8 @@ from openpyxl import Workbook
 import re
 
 #--------------------------------------------- Input Settings -------------------------------------------------
-tail = 2
-Week = 45
+tail = 7
+Week = 47
 cmp = Week - 1
 order1 = ['Main', 'STR', 'SORP']    # This week
 order2 = ['Main', 'STR', 'SORP']    # Last week
@@ -12,7 +12,7 @@ output_order = ['Main', 'STR', 'SORP']
 # order2 = ['Main', 'STR', 'Main']    # w-2 week
 # output_order = ['Main', 'STR', 'Production']
 
-folder = 'C:\\Users\\Yvonne\\Documents\\Results\\'
+folder = 'C:\\Users\\YuFang\\Downloads\\Results'
 #--------------------------------------------------------------------------------------------------------------
 
 def matching(wb1, wb2, wb_out, tail, order1, order2, output_order):
@@ -23,20 +23,23 @@ def matching(wb1, wb2, wb_out, tail, order1, order2, output_order):
         ws = wb_out.create_sheet(output_order[j])
         i = 0
         for row1 in sheet1.iter_rows(max_col=tail, values_only=True):
-            cell_output = cell_data(row1)
+            cell_temp1 = cell_data(row1)
+            cell_output = cell_temp1[0:2]
             match = False
             if cell_output[0] == 'none':
                 break
-            for row2 in sheet2.iter_rows(max_col=2, values_only=True):
-                cell_temp = cell_data(row2)
-                if str(cell_output[0]).lower() == str(cell_temp[0]).lower():
+            for row2 in sheet2.iter_rows(max_col=7, values_only=True):
+                cell_temp2 = cell_data(row2)
+                if str(cell_output[0]).lower() == str(cell_temp2[0]).lower():
                     match = True
-                    cell_output.append(cell_temp[1])
+                    cell_output.append(cell_temp2[1])
                     break
             if match == False:
                 cell_output.append('none')
             i = i + 1
             cell_output.append('=EQ(Left($B'+ str(i) + ',4),Left($C' + str(i) + ',4))')
+            cell_output.append(cell_temp1[6])
+            cell_output.append(cell_temp2[6])
             ws.append(cell_output)
         ws["D1"] = 'Same'
         print(output_order[j] + ': ' + str(i-1))
