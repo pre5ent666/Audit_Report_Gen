6df3ebc9e08a25b714dfcd08c2d7c8ed897d23a9
pre5ent666 <75908533+pre5ent666@users.noreply.github.com>
2022-10-05 17:52:45 +0800

    Add files via upload

--- Audit_Report_Gen.py
+++ Audit_Report_Gen.py
@@ -2,15 +2,17 @@ from openpyxl import load_workbook, workbook
 from openpyxl import Workbook
 import re
 import glob
+import pathlib
 
 #--------------------------------------------- Input Settings -------------------------------------------------
-Week = 202235
-Cmp = 202234
-order1 = ['Main_Resume']    # This week
-order2 = ['Main_Resume']    # Last week
-output_order = order1
+Week1 = 202235
+Week2 = 202234
+order1 = ['Main_Resume']    # sheet name of Week1_Sorted.xlsx
+order2 = ['Main_Resume']    # sheet name of Week2_Sorted.xlsx
+output_order = order1       # sheet name of output file
+Enable_Productlines_Compare = False
 
-folder = 'C:\\Users\\Yvonne\\Documents\\Results'
+folder = str(pathlib.Path(__file__).parent.resolve())
 tail = 4
 #--------------------------------------------------------------------------------------------------------------
 
@@ -31,11 +33,11 @@ def Results_Sum(wb, ws, path):
      + '\nTotal:' + str(j)\
      + '\n=====================================================================================\n')
 
-def Results_All(Order,Week):
+def Results_All(Order,Week1):
     output = Workbook() # Result Summary
     for Line_Name in Order:
         print('# ' + Line_Name + ' #')
-        Title = 'W' + (str(Week)[4:]).zfill(2) + '_' + Line_Name
+        Title = 'W' + (str(Week1)[4:]).zfill(2) + '_' + Line_Name
         ws = output.create_sheet(Line_Name, 0) # create sheet for each line
         row_title = ['Original GM TC ID', Title] 
         ws.append(row_title) # add title
@@ -44,7 +46,7 @@ def Results_All(Order,Week):
         for xls in files :
             wb =  load_workbook(xls)
             Results_Sum(wb, ws, xls)
-    output.save(folder + '\\All\\' + str(Week)[:4]+ '_W' + (str(Week)[4:]).zfill(2) + '.xlsx')
+    output.save(folder + '\\All\\' + str(Week1)[:4]+ '_W' + (str(Week1)[4:]).zfill(2) + '.xlsx')
 
 def matching(sheet1, sheet2, sheet_out, tail):
     i = 0
@@ -80,16 +82,20 @@ def cell_data(row):
                 cells.append(cell)
         return cells
 
-Results_All(order1,Week)
-Results_All(order2,Cmp)
-wb1 = load_workbook(folder + '\\All\\'+ str(Week)[:4]+ '_W' + (str(Week)[4:]).zfill(2) + '.xlsx')
-wb2 = load_workbook(folder + '\\All\\'+ str(Cmp)[:4]+ '_W' + (str(Cmp)[4:]).zfill(2) + '.xlsx')
+# Put all sheets together
+Results_All(order1,Week1)
+Results_All(order2,Week2)
+wb1 = load_workbook(folder + '\\All\\'+ str(Week1)[:4]+ '_W' + (str(Week1)[4:]).zfill(2) + '.xlsx')
+wb2 = load_workbook(folder + '\\All\\'+ str(Week2)[:4]+ '_W' + (str(Week2)[4:]).zfill(2) + '.xlsx')
+
+# Matching results
 print("Matching results......")
 wb_out = Workbook()
+# - base on week
 for j in range(len(order1)):
-    sheet1 = wb1[order1[j]]
-    sheet2 = wb2[order2[j]]
-    sheet_out = wb_out.create_sheet(output_order[j], 0)
-    matching(sheet1, sheet2, sheet_out, tail)
-# matching(wb1[order1[0]], wb1[order1[1]], wb_out.create_sheet("Weekly_lines"), tail)
-wb_out.save(folder + '\\Audit_Report\\Audit_Report_W' + (str(Week)[4:]).zfill(2) + '.xlsx')
+    matching(wb1[order1[j]], wb2[order2[j]], wb_out.create_sheet(output_order[j], 0), tail)
+# - base on product line (for week1)
+if Enable_Productlines_Compare:
+    for k in range(len(order1)-1):
+        matching(wb1[order1[k]], wb1[order1[k+1]], wb_out.create_sheet(order1[k]+"_"+order1[k+1], 0), tail)
+wb_out.save(folder + '\\Audit_Report\\Audit_Report_W' + (str(Week1)[4:]).zfill(2) + '.xlsx')
